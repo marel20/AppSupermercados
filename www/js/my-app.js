e2dd879cb6d09614645a1e3cb3cf3fc1aef959a9
marel20 <marelli_11@hotmail.com>
2021-10-21 13:22:39 -0300

    Modificacion de codigo

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,37 +15,93 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+      {path: '/carrito/', url: 'carrito.html',},
+      {path: '/index/', url: 'index.html',},
+      {path: '/busqueda/', url: 'busqueda.html',},
+      {path: '/categorias/', url: 'categorias.html',},
+      {path: '/cuenta/', url: 'cuenta.html',},
+      {path: '/resumen/', url: 'resumen.html',},
     ]
     // ... other parameters
   });
 
 var mainView = app.views.create('.view-main');
 
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
-});
-
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
     console.log(e);
+
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="carrito"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
-    alert('Hello');
+    $$('#suma').on('click', fnSumar);
 })
 
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="busqueda"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="categorias"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="cuenta"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="resumen"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name=""]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name=""]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+
+})
 
 
 
 
 /*Mis Funciones*/
 
+function fnSumar() {
+  console.log('entramos en sumar')
+  objeto = $$('#descripcion').val();
+  cantidad = $$('#cantidad').val();
+  precio = $$('#unitario').val();
+
+  $$('#col1').append(objeto + "<br>");
+  $$('#col2').append(cantidad + "<br>");
+  $$('#col3').append(precio + "<br>");
+  $$('#col4').append(cantidad * precio + "<br>");
+  
+  canTotal = parseInt($$('#totalArt').text());
+  canTotal+= parseInt(cantidad);
+  $$('#totalArt').text(canTotal);
+
+  pesosTotal = parseInt($$('#totalPesos').text());
+  pesosTotal+= parseInt((cantidad * precio));
+  $$('#totalPesos').text(pesosTotal);
+
+
+
+}
